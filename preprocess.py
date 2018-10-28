fb601e0fcfd6c7887b95c7e5c670acff6872f73e
Lukas Behrendt <lukas.behrendt@student.hpi.de>
2018-10-28 14:35:49 +0100

    Convert mails to Python dict

--- preprocess.py
+++ preprocess.py
@@ -3,32 +3,42 @@
 import sys
 import os
 import re
+import pickle
 
 MEGADIR = "megadir"
 
+
 def main():
-    regex = re.compile(r"^Message-ID:.*$", flags=re.MULTILINE)
-    regex2 = re.compile(r"^X-Folder:.*$", flags=re.MULTILINE)
-    regex3 = re.compile(r"^X-FileName:.*$", flags=re.MULTILINE)
-    regex4 = re.compile(r"^X-Origin:.*$", flags=re.MULTILINE)
+    os.makedirs(MEGADIR, exist_ok=True)
+
+    head_body_regex = re.compile(r"^(.*?\n\n)(.*)", flags=re.S)
+    key_val_regex = re.compile(r"^(.*): (.*)$", flags=re.MULTILINE)
 
     root = sys.argv[1]
     for dirname, _, files in os.walk(root):
-        for path in [os.path.join(dirname, filename) for filename in files]:
-            string = ""
+        for filename in files:
+            path = os.path.join(dirname, filename)
             try:
                 with open(path, 'r') as f:
                     string = f.read()
-                    string = regex.sub("", string)
-                    string = regex2.sub("", string)
-                    string = regex3.sub("", string)
-                    string = regex4.sub("", string)
+                    head_body_match = head_body_regex.match(string)
+                    head_string = head_body_match.group(1)
+                    body_string = head_body_match.group(2)
+
+                    data_dict = dict(key_val_regex.findall(head_string))
+                    data_dict['Body'] = body_string
+                    del data_dict['Message-ID']
+                    del data_dict['X-Folder']
+                    del data_dict['X-FileName']
+                    del data_dict['X-Origin']
 
                 new_path = os.path.join(MEGADIR, path.replace('/', '-'))
-                with open(new_path, 'w') as f:
-                    f.write(string)
-            except:
-                print(path)
+                with open(new_path, 'wb') as f:
+                    pickle.dump(data_dict, f)
+            except Exception as e:
+                print(e)
+            exit()
+
 
 if __name__ == "__main__":
     main()
