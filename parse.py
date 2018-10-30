a026efa8005d09188e1336fe4ea299a20c7da731
Lukas Behrendt <lukas.behrendt@student.hpi.de>
2018-10-30 12:55:37 +0100

    Fix parsing of multiline subjects

--- parse.py
+++ parse.py
@@ -17,7 +17,7 @@ def main():
     os.makedirs(megadir, exist_ok=True)
 
     head_body_regex = re.compile(r"^(.*?\r\n\r\n)(.*)", flags=re.S)
-    key_val_regex = re.compile(r"^(.*?): (.*)$", flags=re.MULTILINE)
+    key_val_regex = re.compile(r"(^|(?<=\r\n))(\S.*?): (.*)\r\n")
 
     for dirname, _, files in os.walk(root):
         for filename in files:
@@ -25,16 +25,20 @@ def main():
             try:
                 with codecs.open(path, 'r', 'cp1252') as f:
                     string = f.read()
-                    head_body_match = head_body_regex.match(string)
-                    head_string = head_body_match.group(1).replace('\r', '')
-                    body_string = head_body_match.group(2).replace('\r', '')
 
-                    data_dict = dict(key_val_regex.findall(head_string))
-                    data_dict['Body'] = body_string
-                    del data_dict['Message-ID']
-                    del data_dict['X-Folder']
-                    del data_dict['X-FileName']
-                    del data_dict['X-Origin']
+                head_body_match = head_body_regex.match(string)
+                head_string = head_body_match.group(1)
+                body_string = head_body_match.group(2).replace('\r', '')
+
+                data_dict = {k: v for _, k, v in key_val_regex.findall(head_string)}
+                if not data_dict:
+                    print(f"Error in {path}: couldn't parse metadata")
+
+                data_dict['Body'] = body_string
+                del data_dict['Message-ID']
+                del data_dict['X-Folder']
+                del data_dict['X-FileName']
+                del data_dict['X-Origin']
 
                 new_path = os.path.join(megadir, path.replace('/', '-'))
                 with open(new_path, 'wb') as f:
--- prepare.sh
+++ prepare.sh
@@ -3,6 +3,6 @@
 script_dir=$(dirname "$0")
 megadir="$1/megadir"
 
-"$script_dir"/replace.sh "$1"
-python3 "$script_dir"/parse.py "$1" "$megadir"
+"$script_dir/replace.sh" "$1"
+python3 "$script_dir/parse.py" "$1" "$megadir"
 fdupes -dN "$megadir"
