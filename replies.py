ced7f29ee1e6d0f7527f2e6e3ebc2692a870dfe3
Oliver Clasen <oliver.clasen@alice-dsl.net>
2018-11-01 17:44:17 +0100

    changed to work on dictionaries, still has to be tested

--- replies.py
+++ replies.py
@@ -19,17 +19,16 @@ reply_body_regex = re.compile(r".*?>\n>(.*?)\n\n", flags=re.MULTILINE|re.S)
 
 root = sys.argv[1]
 for dirname, _, files in os.walk(root):
-    print(dirname)
     for filename in files:
         path = os.path.join(dirname, filename)
         try:
-            with codecs.open(path, 'r',"cp1252") as f:
-                string = f.read()
+            with open(path, 'r',"cp1252") as f:
                 dictionary_actual = pickle.load(f)
+                body = dictionary_actual["Body"]
 
                 potential_mail_places = list()
 
-                subject_regex_match = subject_regex.search(string)
+                subject_regex_match = subject_regex.search(body)
                 if subject_regex_match:
                     print(filename)
                     if subject_regex_match.group(2):
@@ -38,65 +37,60 @@ for dirname, _, files in os.walk(root):
                         subject = subject_regex_match.group(1)
                     print(subject)
 
-                from_regex_match = from_regex.search(string)
-                from_regex2_match = from_regex2.search(string)
-                from_regex3_match = from_regex3.search(string)
+                from_regex_match = from_regex.search(body)
+                from_regex2_match = from_regex2.search(body)
+                from_regex3_match = from_regex3.search(body)
 
                 if from_regex_match:
                     from1 = from_regex_match.group(1)
                     print("From:"+from1)
                     senders = glob.glob('../megadir/'+from1+'*.txt')
-                    potential_mail_places = senders
+                    potential_mail_places.add(senders)
 
                 elif from_regex2_match:
                     from1 = from_regex2_match.group(1)
                     print("From:"+from1)
                     senders = glob.glob('../megadir/'+from1+'*.txt')
-                    potential_mail_places = senders
+                    potential_mail_places.add(senders)
 
                 elif from_regex3_match:
                     from1 = from_regex3_match.group(1)
                     print("From:"+from1)
                     senders = glob.glob('../megadir/'+from1+'*.txt')
-                    potential_mail_places = senders
+                    potential_mail_places.add(senders)
 
-                to_regex_match = to_regex.search(string)
-                to_regex2_match = to_regex2.search(string)
-                to_regex3_match = to_regex3.search(string)
+                to_regex_match = to_regex.search(body)
+                to_regex2_match = to_regex2.search(body)
+                to_regex3_match = to_regex3.search(body)
 
                 if to_regex_match:
                     to1 = to_regex_match.group(1)
                     print("To:"+to1)
                     receivers = glob.glob('../megadir/'+to1+'*.txt')
-                    potential_mail_places = potential_mail_places + receivers
+                    potential_mail_places.add(receivers)
 
                 elif to_regex2_match:
                     to1 = to_regex2_match.group(1)
                     print("To:"+to1)
                     receivers = glob.glob('../megadir/'+to1+'*.txt')
-                    potential_mail_places = potential_mail_places + receivers
+                    potential_mail_places.add(receivers)
 
                 elif to_regex3_match:
                     to1 = to_regex3_match.group(1)
                     print("To:"+to1)
                     receivers = glob.glob('../megadir/'+to1+'*.txt')
-                    potential_mail_places = potential_mail_places + receivers
+                    potential_mail_places.add(receivers)
 
-                reply_body_regex_match = reply_body_regex.search(string)
+                reply_body_regex_match = reply_body_regex.search(body)
                 if reply_body_regex_match:
                     reply_body = reply_body_regex_match.group(1)
 
                 for file in potential_mail_places:
                     path = os.path.join(dirname, filename)
-                        with codecs.open(path, 'r',"cp1252") as compare:
-                            string = compare.read()
-                            dictionary_compare = pickle.load(compare)
-                            if reply_body==dictionary_compare["Body"]:
-                                print("MATCH in: " + file)
-                                dictionary_actual["repliedTo"] = file
-
-
-
-
+                    with open(path, 'r',"cp1252") as compare:
+                        dictionary_compare = pickle.load(compare)
+                        if reply_body==dictionary_compare["Body"]:
+                            print("MATCH in: " + file)
+                            dictionary_actual["repliedTo"] = file
         except Exception as e:
             print(e)
