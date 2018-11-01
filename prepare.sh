774a82c5d71c2a9bf5cde296c870b1cfe2792455
Lukas Behrendt <lukas.behrendt@student.hpi.de>
2018-11-01 16:34:54 +0100

    Create megadir in parent of maildir

--- prepare.sh
+++ prepare.sh
@@ -6,7 +6,7 @@ if [ "$#" -ne 1 ]; then
 fi
 
 script_dir=$(dirname "$0")
-megadir="$1/megadir"
+megadir="$1/../megadir"
 
 "$script_dir/replace.sh" "$1"
 python3 "$script_dir/parse.py" "$1" "$megadir"
