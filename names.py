80be40b5875d3dd902e6d5a98598cf4c18294f1e
Lukas Behrendt <lukas.behrendt@student.hpi.de>
2018-11-02 01:34:31 +0100

    Clean up chosen names by putting name before surname, removing quotes

--- names.py
+++ names.py
@@ -7,7 +7,7 @@ from tqdm import tqdm
 X_KEYS = [("To", "X-To"), ("Cc", "X-cc"), ("Bcc", "X-bcc")]
 name_str = "[A-Z][a-z]+(-[A-Z][a-z]+)?"
 double_name_str = f"{name_str} ({name_str}|[A-Z]\.?)"
-surname_str = f"{name_str}(,? [JS]r.?)?"
+surname_str = f"{name_str}(,? [JS]r\.?)?"
 
 double_name_regex = re.compile(f"({double_name_str} {surname_str}|{surname_str}, {double_name_str})$")
 full_name_regex = re.compile(f"({name_str} {surname_str}|{surname_str}, {name_str})$")
@@ -24,6 +24,25 @@ def choose_best_name(name_map):
             elif simple_name_regex.match(name):
                 names[name] *= 1.1
         chosen_names[address] = max(names, key=names.get)
+    return chosen_names
+
+def cleanup_names(chosen_names):
+    for address in chosen_names:
+        name = chosen_names[address]
+        if not name: continue
+
+        if name[0] == '"' and name[-1] == '"':
+            name = name[1:-1]
+        if name[0] == "'" and name[-1] == "'":
+            name = name[1:-1]
+
+        split_name = name.split(',', 1)
+        if len(split_name) > 1 and not re.match(" [JS]r\.?", split_name[0]):
+            name = f"{split_name[1]} {split_name[0]}".strip()
+
+        # if chosen_names[address] != name:
+            # print(name)
+        chosen_names[address] = name
     return chosen_names
 
 def main():
@@ -49,9 +68,13 @@ def main():
     # p_chosen_names = {address: max(names, key=names.get) for address, names in name_map.items()}
     chosen_names = choose_best_name(name_map)
 
-    # for address in name_map:
+    # for address in chosen_names:
         # if chosen_names[address] != p_chosen_names[address]:
             # print(f"{address}: {chosen_names[address]} instead of {p_chosen_names[address]}")
+
+    chosen_names = cleanup_names(chosen_names)
+    # for address in chosen_names:
+        # print(f"{address}: {chosen_names[address]}")
 
 if __name__ == '__main__':
     main()
